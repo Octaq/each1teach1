f093c3583210bc7ab89bbb2793b7e45ce30c869e
Tokelo Seremane <seremane.t@gmail.com>
2017-11-27 18:31:37 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+	<title>Each1Teach1</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+	<link href="css/style.css" rel="stylesheet" type="text/css" />
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<link href="css/normalize.css" rel="stylesheet" type="text/css" />
+	<link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet"> 
+	<!-- <link href="css/animate.css" rel="stylesheet" type="text/css" /> -->
+	<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
+</head>
+
+<body>
+	 <div class="container-fluid">
+		 <div id="hero" class="jumbotron ">
+	 		<div class="row">
+                    <div class="col-xs-12 col-md-1"></div>
+	 				<div class=" col-xs-12 col-md-6">
+	 					<div class="logo">
+	 					    <img src="../media/Sprites/ETO_FullLogo1.png" height="150"/>
+	 					</div>
+	 					<p>
+	 					    Each one teach one, each one reach one until all are taught and have the necessary information they need to succeed. Each1Teach1 is a gathering of
+                                minds looking at one subject and learning from each other. 
+                        <a href="#demo"  data-toggle="collapse"><h6>(read more…).</h6></a>
+                            <div id="demo" class="collapse">
+                                This is a primary platform for students, professionals and the society to connect with each other and share knowledge and information. Students struggling with certain topics of a course or a subject will be able communicate with students who can assist them with that specific topics or subject. This platform allow users to communicate and create lifetime networks aimed at the sharing of information, career guidance from each other and from people already in the working industry..
+                            </div>
+
+	 						</p>
+	 						<a href="pages/login.php" id="default-btn" class="btn btn-info" role="button">Login</a>
+	 						<a href="pages/register2.php" id="default-btn" class="btn btn-info" role="button">Register</a>
+	 				</div>
+	 				 <div class="col-xs-12 col-md-4"></div>
+	 		</div>
+	 	</div>
+	 </div>
+<!-- /.scripts -->
+	<script src="js/script.js"></script>
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+<!-- /.scripts -->
+</body>
+</html>
+
